50cf9be2f822c9ddb4cef2dc38ffee06a0f6b7fc
BuildTools <73603426+CaptainMudkip1@users.noreply.github.com>
2021-12-24 11:50:49 -0500

    Finished grade calculator and added a cool title to the projects page.

--- grade_calc/index.js
+++ grade_calc/index.js
@@ -1,15 +1,17 @@
 function gradeCalc(earned, total) {
   pe = earned * 100;
   x = pe / total;
-  x = x + "%"
   return x
 }
 function CALC() {
   e = document.getElementById("earned").value;
   t = document.getElementById("total").value;
-  x = gradeCalc(e, t);
-  document.clear();
-  fetch('./index.html')
-    .then(response => response.text())
-    .then((data) => {document.write(data + '<p>Your grade was <b>' + x + '</b></p>')})
+  o = document.getElementById("outputText");
+  if(e == ""){o.innerHTML = "";}
+  else if(t == ""){o.innerHTML = "";}
+  else {
+    x = gradeCalc(e, t);
+    console.log(x);
+    o.innerHTML = "Your score was <b>" + x + "%</b>";
+  }
 }
