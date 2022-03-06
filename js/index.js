7e8695651163debd3deddacbe996a47ccd047aa9
CaptainMudkip1 <73603426+CaptainMudkip1@users.noreply.github.com>
2022-03-06 16:39:50 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,3 @@
 function copyIP() {
   navigator.clipboard.writeText("play.bankofdog.ml");
 }
-
-function onLoadDynmap() {
-  document.getElementsByClassName("dynmap-iframe").src = "http://bankofdog.my.pebble.host:8067/index.html";
-  document.getElementsByClassName("dynmap-iframe").contentWindow.location.reload();
-}
